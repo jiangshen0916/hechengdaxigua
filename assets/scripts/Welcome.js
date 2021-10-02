8fbc260d7ec5e8e53a38736b9a779bba0a025540
jiangshen <3045569907@qq.com>
2021-10-03 01:42:17 +0800

    合成大西瓜

---
+++ assets/scripts/Welcome.js
@@ -0,0 +1,22 @@
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+
+    },
+    
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    // 跳转场景
+    onStaBtn(){
+        cc.director.loadScene("game")
+    }
+
+    // update (dt) {},
+});
